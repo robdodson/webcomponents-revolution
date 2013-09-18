d6e34d9c7445ca46a0c673f3f3f430042bbec988
Rob Dodson <lets.email.rob@gmail.com>
2013-09-18 16:12:04 -0700

    interim commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function () {
+  // var showPie = function showPie(e) {
+  //   if (Boolean(e.fragment.getAttribute('data-pie'))) {
+  //     var slide = Reveal.getCurrentSlide();
+  //     var pie = document.createElement('chart-pie');
+  //     slide.appendChild(pie);
+  //     Reveal.removeEventListener('fragmentshown', showPie);
+  //   }
+  // };
+
+  Reveal.addEventListener('fragmentshown', function() {
+    console.log(Reveal.getCurrentSlide());
+  });
+}());
