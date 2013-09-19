1c278aba1fccb575877c3bad0cd95c6e71b5af36
Rob Dodson <lets.email.rob@gmail.com>
2013-09-19 15:43:54 -0700

    interim commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-(function () {
+(function (window, document, undefined) {
+
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode === 74) { // j key
+      var fragments = Reveal.getCurrentSlide().querySelectorAll('.fragment');
+      Array.prototype.forEach.call(fragments, function(fragment) {
+        fragment.classList.toggle('force-focus');
+      });
+    }
+  });
+
+  // <chart-pie> demo
   var showPie = function showPie(e) {
     if (Boolean(e.fragment.getAttribute('data-pie'))) {
       var slide = Reveal.getCurrentSlide();
@@ -9,4 +20,24 @@
   };
 
   Reveal.addEventListener('fragmentshown', showPie);
-}());
+
+
+  var demos = {};
+
+  demos.template = function template() {
+    var section = Reveal.getCurrentSlide();
+    var template = section.querySelector('#my-widget');
+    template.content.querySelector('img').src = 'http://localhost:3000/img/corgi.gif';
+    section.appendChild(template.content.cloneNode(true));
+  };
+
+  demos.shadowRoot = function shadowRoot() {
+    var section = Reveal.getCurrentSlide();
+    var host = section.querySelector('.widget');
+    var root = host.createShadowRoot();
+    root.textContent = 'Im inside yr div!';
+  };
+
+  window.demos = demos;
+
+}(window, document));
